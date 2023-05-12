1c577a35f7ef787e036ca6270a9d21fec2422926
Satwinder04 <satwindersalan@gmail.com>
2023-05-12 21:23:39 +0530

    podcast card done

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,11 +32,11 @@ function Home() {
                 </div>
                 <div className="card-box ">
                   <div className="col-md-12 m-auto d-flex">
-                    <div className="col-md-2 mx-4">
+                    <div className="col-md-2 mx-5">
                       <img className=" banner-img" src="../img/like.jpg" />
                     </div>
                     <div
-                      className="col-md-10 text-light d-flex flex-column justify-content-between py-3"
+                      className=" text-light d-flex flex-column justify-content-between py-3 "
                       style={{ height: "30vh" }}
                     >
                       <p className="banner-head mt-4">Play List</p>
@@ -57,6 +57,48 @@ function Home() {
                   </div>
                 </div>
                 <MusicLists />
+
+                <div className="card-box">
+                  <div className="d-flex flex-column w-full px-2 m-4">
+                    <div className="">
+                      <p className="h3 text-start text-light">Browse all</p>
+                    </div>
+                    <div className="col-md-12 my-4 d-flex flex-wrap">
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                      <div className="card3 col-md-4"  style={{ background: "rgb(225, 51, 0)" }}>
+                        <p className="h4 p-3 text-light">Podcast</p>
+                        <img className="col-md-7 mx-auto " src="../img/podcast.jpg" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
