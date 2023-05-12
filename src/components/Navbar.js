4eba80aa4e180373c05b003b351e5c09cc1fd69e
Satwinder04 <satwindersalan@gmail.com>
2023-05-12 23:16:08 +0530

    search added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,40 @@ function Navbar(props) {
     <>
       <div className="s-navbar my-auto d-flex align-item-center justify-content-between">
         <div className="s-nav-button d-flex my-3 mx-2">
-          <img src="../img/l-arw.jpg" />
-          <img src="../img/r-arw.jpg" />
+          <div className="d-flex my-auto">
+            <img src="../img/l-arw.jpg" />
+            <img src="../img/r-arw.jpg" />
+          </div>
+          <div className="mx-3 my-auto">
+            <div className="container-input">
+              <input
+                type="text"
+                placeholder="What do you want to listen to ?"
+                name="text"
+                className="input"
+              />
+              <svg
+                fill="#000000"
+                width="20px"
+                height="20px"
+                viewBox="0 0 1920 1920"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
         <div className="d-flex my-3 mx-0 ">
-
-        <button type="button" class="upgrade btn btn-outline-light fw-bold rounded-pill px-3">
-          Upgrade</button>
-
-
+          <button
+            type="button"
+            class="upgrade btn btn-outline-light fw-bold rounded-pill px-3"
+          >
+            Upgrade
+          </button>
 
           <div class="dropdown mx-4">
             <button
@@ -21,9 +46,10 @@ function Navbar(props) {
               type="button"
               data-bs-toggle="dropdown"
               aria-expanded="false"
-            ><img className="dp" src="../img/sattu.jpg"/>
-              <span className="mx-2">Sattu</span> 
-              <img className="d-arrow" src="../img/downward-arrow.png"/>
+            >
+              <img className="dp" src="../img/sattu.jpg" />
+              <span className="mx-2">Sattu</span>
+              <img className="d-arrow" src="../img/downward-arrow.png" />
             </button>
             <ul class="dropdown-menu dropdown-menu-dark">
               <li>
@@ -51,7 +77,7 @@ function Navbar(props) {
                   Setting
                 </a>
               </li>
-              <hr/>
+              <hr />
               <li>
                 <a class="dropdown-item" href="#">
                   Loh Out
--- src/pages/MusicLists.js
+++ src/pages/MusicLists.js
@@ -10,13 +10,15 @@ function MusicLists(props) {
               <th className=" px-2 pb-4 fw-normal ">#</th>
               <th className=" px-2 pb-4 fw-normal ">TITLE</th>
               <th className=" px-2 pb-4 fw-normal ">ALBUM</th>
-              <th className=" px-2 pb-4 fw-normal ">DATE ADDED</th>
-              <th className=" px-2 pb-4 fw-normal ">
+              <th className=" px-5 pb-4 fw-normal ">DATE ADDED</th>
+              <th className=" px-0 pb-4 fw-normal "></th>
+              <th className=" px-0 pb-4 fw-normal ">
                 <img className="table1-icon" src="../img/time.png" />
               </th>
             </tr>
           </thead>
           <tbody>
+            
             <tr className="col-md-12 ">
               <td className="px-2 py-2 align-middle">1</td>
               <td className="px-2 py-2 align-middle d-flex  ">
@@ -28,9 +30,30 @@ function MusicLists(props) {
                 </div>
               </td>
               <td className="px-2 py-2 align-middle">Ishq</td>
-              <td className="px-2 py-2 align-middle">20 days ago</td>
-              <td className="px-2 py-2 align-middle">7:11</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
             </tr>
+            
             <tr className="col-md-12 ">
               <td className="px-2 py-2 align-middle">1</td>
               <td className="px-2 py-2 align-middle d-flex  ">
@@ -42,9 +65,30 @@ function MusicLists(props) {
                 </div>
               </td>
               <td className="px-2 py-2 align-middle">Ishq</td>
-              <td className="px-2 py-2 align-middle">20 days ago</td>
-              <td className="px-2 py-2 align-middle">7:11</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
             </tr>
+            
             <tr className="col-md-12 ">
               <td className="px-2 py-2 align-middle">1</td>
               <td className="px-2 py-2 align-middle d-flex  ">
@@ -56,9 +100,30 @@ function MusicLists(props) {
                 </div>
               </td>
               <td className="px-2 py-2 align-middle">Ishq</td>
-              <td className="px-2 py-2 align-middle">20 days ago</td>
-              <td className="px-2 py-2 align-middle">7:11</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
             </tr>
+            
             <tr className="col-md-12 ">
               <td className="px-2 py-2 align-middle">1</td>
               <td className="px-2 py-2 align-middle d-flex  ">
@@ -70,9 +135,30 @@ function MusicLists(props) {
                 </div>
               </td>
               <td className="px-2 py-2 align-middle">Ishq</td>
-              <td className="px-2 py-2 align-middle">20 days ago</td>
-              <td className="px-2 py-2 align-middle">7:11</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
             </tr>
+            
             <tr className="col-md-12 ">
               <td className="px-2 py-2 align-middle">1</td>
               <td className="px-2 py-2 align-middle d-flex  ">
@@ -84,8 +170,98 @@ function MusicLists(props) {
                 </div>
               </td>
               <td className="px-2 py-2 align-middle">Ishq</td>
-              <td className="px-2 py-2 align-middle">20 days ago</td>
-              <td className="px-2 py-2 align-middle">7:11</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
+            </tr>
+            
+            <tr className="col-md-12 ">
+              <td className="px-2 py-2 align-middle">1</td>
+              <td className="px-2 py-2 align-middle d-flex  ">
+                <img className="song-img" src="../img/Ishq.jpg" />
+
+                <div className="align-middle d-flex flex-column">
+                  <p className=" text-light song-title">Ishq</p>
+                  <p className="">Nirvair Pannu Nirvair Pannu </p>
+                </div>
+              </td>
+              <td className="px-2 py-2 align-middle">Ishq</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
+            </tr>
+            
+            <tr className="col-md-12 ">
+              <td className="px-2 py-2 align-middle">1</td>
+              <td className="px-2 py-2 align-middle d-flex  ">
+                <img className="song-img" src="../img/Ishq.jpg" />
+
+                <div className="align-middle d-flex flex-column">
+                  <p className=" text-light song-title">Ishq</p>
+                  <p className="">Nirvair Pannu Nirvair Pannu </p>
+                </div>
+              </td>
+              <td className="px-2 py-2 align-middle">Ishq</td>
+              <td className="px-5 py-2 align-middle">20 days ago</td>
+
+              <td className="px-0 py-2 align-middle ">
+                <label className="container">
+                  <input defaultChecked="checked" type="checkbox" />
+                  <div className="checkmark">
+                    <svg viewBox="0 0 256 256">
+                      <rect fill="none" height={256} width={256} />
+                      <path
+                        d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                        strokeWidth="20px"
+                        stroke="#FFF"
+                        fill="none"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </td>
+
+              <td className="px-0 py-2 align-middle">
+                <p className="px-auto">7:11</p>
+              </td>
             </tr>
           </tbody>
         </table>
