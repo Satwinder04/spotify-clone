597921d0d2babbb3fdb1b48f2e219ce9a4927b3b
Satwinder04 <satwindersalan@gmail.com>
2023-05-12 12:36:22 +0530

    table completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,6 +32,103 @@ function Home() {
                   <VerticalCard />
                   {/* </div> */}
                 </div>
+                <div className="card-box">
+                  <div>
+                    <table className="table-1 my-4 ">
+                      <thead >
+                        <tr >
+                          <th className=" px-2 pb-4 fw-normal ">#</th>
+                          <th className=" px-2 pb-4 fw-normal ">TITLE</th>
+                          <th className=" px-2 pb-4 fw-normal ">ALBUM</th>
+                          <th className=" px-2 pb-4 fw-normal ">DATE ADDED</th>
+                          <th className=" px-2 pb-4 fw-normal "><img className="table1-icon" src="../img/time.png" /></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="col-md-12 "> 
+                          <td className="px-2 py-2 align-middle">1</td>
+                          <td className="px-2 py-2 align-middle d-flex  ">
+                           
+                              <img className="song-img" src="../img/Ishq.jpg" />
+                            
+                            <div className="align-middle d-flex flex-column">
+                              <p className=" text-light song-title">Ishq</p>
+                              <p className="">Nirvair Pannu Nirvair Pannu </p>
+                            </div>
+
+                          </td>
+                          <td className="px-2 py-2 align-middle">Ishq</td>
+                          <td className="px-2 py-2 align-middle">20 days ago</td>
+                          <td className="px-2 py-2 align-middle">7:11</td>
+                        </tr>
+                        <tr className="col-md-12 "> 
+                          <td className="px-2 py-2 align-middle">1</td>
+                          <td className="px-2 py-2 align-middle d-flex  ">
+                           
+                              <img className="song-img" src="../img/Ishq.jpg" />
+                            
+                            <div className="align-middle d-flex flex-column">
+                              <p className=" text-light song-title">Ishq</p>
+                              <p className="">Nirvair Pannu Nirvair Pannu </p>
+                            </div>
+
+                          </td>
+                          <td className="px-2 py-2 align-middle">Ishq</td>
+                          <td className="px-2 py-2 align-middle">20 days ago</td>
+                          <td className="px-2 py-2 align-middle">7:11</td>
+                        </tr>
+                        <tr className="col-md-12 "> 
+                          <td className="px-2 py-2 align-middle">1</td>
+                          <td className="px-2 py-2 align-middle d-flex  ">
+                           
+                              <img className="song-img" src="../img/Ishq.jpg" />
+                            
+                            <div className="align-middle d-flex flex-column">
+                              <p className=" text-light song-title">Ishq</p>
+                              <p className="">Nirvair Pannu Nirvair Pannu </p>
+                            </div>
+
+                          </td>
+                          <td className="px-2 py-2 align-middle">Ishq</td>
+                          <td className="px-2 py-2 align-middle">20 days ago</td>
+                          <td className="px-2 py-2 align-middle">7:11</td>
+                        </tr>
+                        <tr className="col-md-12 "> 
+                          <td className="px-2 py-2 align-middle">1</td>
+                          <td className="px-2 py-2 align-middle d-flex  ">
+                           
+                              <img className="song-img" src="../img/Ishq.jpg" />
+                            
+                            <div className="align-middle d-flex flex-column">
+                              <p className=" text-light song-title">Ishq</p>
+                              <p className="">Nirvair Pannu Nirvair Pannu </p>
+                            </div>
+
+                          </td>
+                          <td className="px-2 py-2 align-middle">Ishq</td>
+                          <td className="px-2 py-2 align-middle">20 days ago</td>
+                          <td className="px-2 py-2 align-middle">7:11</td>
+                        </tr>
+                        <tr className="col-md-12 "> 
+                          <td className="px-2 py-2 align-middle">1</td>
+                          <td className="px-2 py-2 align-middle d-flex  ">
+                           
+                              <img className="song-img" src="../img/Ishq.jpg" />
+                            
+                            <div className="align-middle d-flex flex-column">
+                              <p className=" text-light song-title">Ishq</p>
+                              <p className="">Nirvair Pannu Nirvair Pannu </p>
+                            </div>
+
+                          </td>
+                          <td className="px-2 py-2 align-middle">Ishq</td>
+                          <td className="px-2 py-2 align-middle">20 days ago</td>
+                          <td className="px-2 py-2 align-middle">7:11</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -79,41 +176,39 @@ function Home() {
             </div>
           </div>
           <div className=" volume ">
-            
-              <div className="footer-icon-2 mx-0">
-                <img src="../img/mic.png" />
-              </div>
-              <div className="footer-icon-2 mx-0">
-                <img src="../img/queue.png" />
-              </div>
-              <div className="footer-icon-2 mx-0">
-                <img src="../img/pc.png" />
-              </div>
-              <div className="footer-icon-2 mx-0">
-                <img src="../img/sound.png" />
-              </div>
-              <div className="my-auto w-5 mx-0 px-2">
-                {/* <input type="range" min={1} max={100} defaultValue={50} style={{width:"6vw", height: "5px" }}/> */}
+            <div className="footer-icon-2 mx-0">
+              <img src="../img/mic.png" />
+            </div>
+            <div className="footer-icon-2 mx-0">
+              <img src="../img/queue.png" />
+            </div>
+            <div className="footer-icon-2 mx-0">
+              <img src="../img/pc.png" />
+            </div>
+            <div className="footer-icon-2 mx-0">
+              <img src="../img/sound.png" />
+            </div>
+            <div className="my-auto w-5 mx-0 px-1">
+              {/* <input type="range" min={1} max={100} defaultValue={50} style={{width:"6vw", height: "5px" }}/> */}
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Example 1px high"
+                aria-valuenow={25}
+                aria-valuemin={0}
+                aria-valuemax={100}
+                style={{
+                  height: "4px",
+                  width: "7vw",
+                  background: "rgb(94, 94, 94)",
+                }}
+              >
                 <div
-                  className="progress"
-                  role="progressbar"
-                  aria-label="Example 1px high"
-                  aria-valuenow={25}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                  style={{
-                    height: "4px",
-                    width: "7vw",
-                    background: "rgb(94, 94, 94)",
-                  }}
-                >
-                  <div
-                    className="progress-bar"
-                    style={{ width: "25%", background: "white" }}
-                  />
-                </div>
+                  className="progress-bar"
+                  style={{ width: "25%", background: "white" }}
+                />
               </div>
-          
+            </div>
           </div>
         </div>
       </div>
