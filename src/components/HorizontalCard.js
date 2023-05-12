0daf654745e67b5ce69ed5b2f3b166738baa1611
Abhishek3139 <abhishekchoudhary3139@gmail.com>
2023-05-12 12:50:34 +0530

    add

--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -4,27 +4,29 @@ import { StateContext } from "../context/context";
 
 function HorizontalCard(props) {
   const { latestData } = useContext(StateContext);
-  // const getTrack = async () => {
-
   console.log(latestData);
   return (
     <>
-      {latestData.splice(0, 6).map((latestData) => {
-        return (
-          <div
-            className="col-md-3 card1 d-flex align-items-center justify-content-between"
-            key={latestData.id}
-          >
-            <div className="col-md-3 ">
-              <img className="card-dp" src={latestData.images[0].url} />
+      {latestData &&
+        latestData.slice(5, 11).map((latestData) => {
+          return (
+            <div
+              className="col-md-3 card1 d-flex align-items-center justify-content-between"
+              key={latestData.id}
+            >
+              <div className="col-md-3 ">
+                <img className="card-dp" src={latestData.images[0].url} />
+              </div>
+              <div className="col-md-9 d-flex align-items-center justify-content-between">
+                <p className="text-start mx-1 text-light">{latestData.name}</p>
+                <img
+                  className="p-3 text-end play-button"
+                  src="../img/play.jpg"
+                />
+              </div>
             </div>
-            <div className="col-md-9 d-flex align-items-center justify-content-between">
-              <p className="text-start mx-1 text-light">{latestData.name}</p>
-              <img className="p-3 text-end play-button" src="../img/play.jpg" />
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </>
   );
 }
--- src/components/VerticalCard.js
+++ src/components/VerticalCard.js
@@ -5,12 +5,17 @@ import { StateContext } from "../context/context";
 function VerticalCard() {
   const { token, getArtistData, artistData } = useContext(StateContext);
   const getRecentPlayed = async () => {
-    const response = await axios(`https://api.spotify.com/v1/me/top/tracks`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
+    const response = await axios(
+      `
+      https://api.spotify.com/v1/me/player/recently-played`,
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
     const { items } = response.data;
+    // console.log(items);
     const artistsData = items.map((artistData) => {
       return artistData;
     });
@@ -27,12 +32,14 @@ function VerticalCard() {
   return (
     <div className="d-flex flex-wrap">
       {artistData &&
-        artistData.slice(0, 5).map((artistData, index) => {
-          const { name, album, artists } = artistData;
+        artistData.slice(10, 15).map((artistData, index) => {
           return (
             <div className="col-md-3 card2" key={index}>
               <div className="col-md-10 m-3" style={{ position: "relative" }}>
-                <img className="card2-img" src={album.images[0].url} />
+                <img
+                  className="card2-img"
+                  src={artistData.track.album.images[0].url}
+                />
                 <img
                   className="v-play-button"
                   src="../img/play.jpg"
@@ -40,8 +47,10 @@ function VerticalCard() {
                 />
               </div>
               <div className="col-md-10 text-light px-0 discription">
-                <h5 className="fw-bold">{name.slice(0, 20)}</h5>
-                <p className="my-2 mb-3">{artists[0].name}</p>
+                <h5 className="fw-bold">
+                  {artistData.track.name.slice(0, 20)}
+                </h5>
+                <p className="my-2 mb-3">{artistData.track.artists[0].name}</p>
               </div>
             </div>
           );
