9b2cb97102bac47564ed13056a91f79c95622472
Satwinder04 <satwindersalan@gmail.com>
2023-05-15 18:59:04 +0530

    horizontal play button fixed

--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -15,15 +15,18 @@ function HorizontalCard(props) {
               key={latestData.id}
             >
               <div className="col-md-3 ">
-                <img className="card-dp" src={latestData.images[0].url} />
+                <img className="card-dp" src="../img/sidhu3.jpg" />
               </div>
-              <div className="col-md-9 d-flex align-items-center justify-content-between">
+              <div className="col-md-6 d-flex align-items-center justify-content-between">
                 <p className="text-start mx-3 text-light">{latestData.name}</p>
-                <img
-                  className="p-3 text-end play-button"
+               
+              </div>
+              <div className="col-md-2 mx-1">
+               <img
+                  className="play-button"
                   src="../img/play.jpg"
                 />
-              </div>
+                </div>
             </div>
           );
         })}
